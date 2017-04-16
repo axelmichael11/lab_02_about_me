db1c0d2618b3ed5918ef89cf160a332e65bf2fc8
Michael Axelson <axelmichael11@gmail.com>
2017-04-16 01:15:04 -0700

    lab three revised, all answers writing to page as well...

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
 
 var correct = 0;
-/*
+
 var correct= 0;
 console.log('suh dude');
 var question1 = 'do you think i like basketball?';
 var question2 = 'do you think soccer is my favorite sport??';
-var question3= 'do you think thai food is my favorite food??';
-var question4= 'do you think my favorite color is blue??';
-var question5= 'do you think I like writing JS?!???!?';
-var question6= 'SOOO what do you think is my favorite number?!?';
-
+var question3 = 'do you think thai food is my favorite food??';
+var question4 = 'do you think my favorite color is blue??';
+var question5 = 'do you think I like writing JS?!???!?';
+var question6 = 'SOOO what do you think is my favorite number?!?';
+var question7 = 'What states have I been to??'
 var right= 'you are right!!!!';
 var wrong= 'you are wrong....';
 
@@ -29,6 +29,7 @@ document.getElementById('question4').innerHTML = question4;
 document.getElementById('question5').innerHTML = question5;
 document.getElementById('question6').innerHTML = question6;
 
+document.getElementById('question7').innerHTML = question7;
 
 if (answer1.trim().toUpperCase()   == 'YES' || answer1.trim() =='y' ) {
   document.getElementById('answer1').innerHTML = right;
@@ -75,10 +76,10 @@ if (answer5.trim().toUpperCase() == 'YES' || answer5.trim() == 'y' ) {
   document.getElementById('answer5').innerHTML = wrong;
   console.log(wrong);
 }
-*/
+
 
 //Question 6
-/*
+
 alert('SOOO what do you think is my favorite number?!? I will give you four tries to get the right answer!!!!');
 
 var right= 'you are right!!!!';
@@ -104,46 +105,28 @@ for (var i = 4; i > 0 && notDone; i--) {
     document.getElementById('answer6').innerHTML = wrong;
   }
 }
-*/
+
 //Question 7
 
-var states= ['Washington','Oregon','Idaho','California','Hawaii','South Carolina','WA','CA','OR','ID','HI','SC'];
+var states= ['Oregon','Idaho','California','Hawaii','South Carolina','CA','OR','ID','HI','SC'];
 
-alert('Last series of questions! I will give you seven attempts to get one question right! The question is, which states have I been to in the United States?!??!?');
+alert('Last series of questions! I will give you seven attempts to get this question right! The question is, which states have I been to in the United States?!??!?');
 
 var notFinished = true;
 
-
-for (var i = 7; i > 0 && notFinished; i--) {
-  console.log('i'+i);
-  for (var j=0; j<states.length; j++) {
-    var question7 = prompt('Which State??!!?');
-    if (question7.trim().toUpperCase() === states[j].trim().toUpperCase()) { //how to switch up all the strings so they are equal.... with lower/upper case...
-      console.log('i in if'+i);
-      console.log('j in if'+j);
-      correct++;
-      alert('yay your right!');
-      console.log(j);
+for (var I = 7; I > 0 && notFinished; I--) {
+  var answer7 = prompt(question7);
+  for (var j = 0; j < states.length; j++) {
+    if (answer7.trim().toUpperCase() === states[j].trim().toUpperCase()) {
+      alert('yeah you got it!');
       notFinished=false;
-      console.log(notFinished);
-    } else {
-      alert('sorry thats not right...');
-      console.log('j'+j);
+      document.getElementById('answer7').innerHTML = right + ' yes of course AHHHHH';
     }
   }
+  if (notFinished) {
+    alert('nope! Guess again...');
+  } else if (I === 1) {
+    alert('nope! Guess again...');
+    document.getElementById('answer6').innerHTML = wrong;
+  }
 }
-//
-// document.getElementById("question7").innerHTML='Last series of questions! I will give you seven attempts to answer! The question is, which states have I been to in the United States?!??!?';
-//
-// document.getElementById("answer7").innerHTML="You got  "+correctstate+" out of seven attempts! Not too bad";
-//
-//
-//
-// //
-// // var answer6= prompt('Last Question! Which States have I been to in the United States?');
-// // var rightanswers= [];
-// //
-// // for (var i= 0; i<states.length; i++) {
-// //   if (answer6===states[i] && correct) {
-// //     rightanswers.push(states[i])
-// //   }
